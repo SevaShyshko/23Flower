c0ce4b9e826cb0494aa7dd56490b4ac6f39076ba
SevaShyshko <102148096+SevaShyshko@users.noreply.github.com>
2025-03-19 22:36:08 +0300

    Add files via upload

---
+++ about us.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    const burgerMenu = document.getElementById('burgerMenu');
+    burgerMenu.classList.toggle('active');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const spoilers = document.querySelectorAll(".spoiler");
+
+    spoilers.forEach(spoiler => {
+        const question = spoiler.querySelector(".spoiler-question");
+        const answer = spoiler.querySelector(".spoiler-answer");
+
+        question.addEventListener("click", () => {
+            answer.classList.toggle("open");
+        });
+    });
+})
+
